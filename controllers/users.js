acee190017dd9a2053500671536f32fe248475e3
ale-lit <zeldor@ya.ru>
2022-03-18 12:52:54 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -138,8 +138,7 @@ module.exports.login = (req, res, next) => {
         httpOnly: true,
         sameSite: true,
       })
-        .status(200)
-        .send({ message: 'Авторизация прошла успешно!' });
+        .end();
     })
     .catch(() => {
       // ошибка аутентификации
