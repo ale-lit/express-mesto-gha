47640cb3adb314a8551b82db815081f796b2be83
ale-lit <zeldor@ya.ru>
2022-03-25 23:15:11 +0300

    update

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
 const cardsRouter = require('express').Router();
-const cors = require('cors');
+// const cors = require('cors');
 const {
   getCards,
   deleteCard,
@@ -10,40 +10,40 @@ const {
 } = require('../controllers/cards');
 const urlRegexpPattern = require('../regexp');
 
-const corseOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
+// const corseOptions = {
+//   origin: '*',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// };
 
-cardsRouter.options('*', cors(corseOptions));
+// cardsRouter.options('*', cors(corseOptions));
 
-cardsRouter.get('/cards', cors(corseOptions), getCards);
+cardsRouter.get('/cards', getCards);
 
-cardsRouter.options('/cards/:id', cors(corseOptions));
-cardsRouter.delete('/cards/:id', cors(corseOptions), celebrate({
+cardsRouter.options('/cards/:id');
+cardsRouter.delete('/cards/:id', celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().alphanum().length(24),
   }),
 }), deleteCard);
 
-cardsRouter.post('/cards', cors(corseOptions), celebrate({
+cardsRouter.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().pattern(urlRegexpPattern),
   }),
 }), createCard);
 
-cardsRouter.options('/cards/:id/likes', cors(corseOptions));
-cardsRouter.put('/cards/:id/likes', cors(corseOptions), celebrate({
+// cardsRouter.options('/cards/:id/likes', cors(corseOptions));
+cardsRouter.put('/cards/:id/likes', celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().alphanum().length(24),
   }),
 }), likeCard);
 
-cardsRouter.options('/cards/:id/likes', cors(corseOptions));
-cardsRouter.delete('/cards/:id/likes', cors(corseOptions), celebrate({
+// cardsRouter.options('/cards/:id/likes', cors(corseOptions));
+cardsRouter.delete('/cards/:id/likes', celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().alphanum().length(24),
   }),
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
 const usersRouter = require('express').Router();
-const cors = require('cors');
+// const cors = require('cors');
 const {
   getUsers,
   getUserMe,
@@ -10,35 +10,35 @@ const {
 } = require('../controllers/users');
 const urlRegexpPattern = require('../regexp');
 
-const corseOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
+// const corseOptions = {
+//   origin: '*',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// };
 
-usersRouter.options('*', cors(corseOptions));
+// usersRouter.options('*', cors(corseOptions));
 
-usersRouter.get('/users', cors(corseOptions), getUsers);
+usersRouter.get('/users', getUsers);
 
-usersRouter.get('/users/me', cors(corseOptions), getUserMe);
+usersRouter.get('/users/me', getUserMe);
 
-usersRouter.get('/users/:id', cors(corseOptions), celebrate({
+usersRouter.get('/users/:id', celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().alphanum().length(24),
   }),
 }), getUser);
 
-usersRouter.options('/users/me', cors(corseOptions));
-usersRouter.patch('/users/me', cors(corseOptions), celebrate({
+// usersRouter.options('/users/me');
+usersRouter.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
 
-usersRouter.options('/users/me/avatar', cors(corseOptions));
-usersRouter.patch('/users/me/avatar', cors(corseOptions), celebrate({
+// usersRouter.options('/users/me/avatar');
+usersRouter.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().pattern(urlRegexpPattern),
   }),
