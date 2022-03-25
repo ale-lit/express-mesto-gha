5d767d3d447fbeaf40e1b9697f595e015be896eb
ale-lit <zeldor@ya.ru>
2022-03-25 23:51:22 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -138,7 +138,7 @@ module.exports.updateUserAvatar = (req, res, next) => {
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
-  res.status(200).send(email + password);
+  res.status(200).send(req.body);
 
   // return User.findUserByCredentials(email, password)
   //   .then((user) => {
