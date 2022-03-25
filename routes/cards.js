b081c6de8f857d67768c00ce6609b142c921732c
ale-lit <zeldor@ya.ru>
2022-03-25 21:09:46 +0300

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -10,25 +10,32 @@ const {
 } = require('../controllers/cards');
 const urlRegexpPattern = require('../regexp');
 
-cardsRouter.options('*', cors());
+// cardsRouter.options('*', cors());
 
 cardsRouter.get('/cards', cors(), getCards);
+
+cardsRouter.options('/cards/:id', cors());
 cardsRouter.delete('/cards/:id', cors(), celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().alphanum().length(24),
   }),
 }), deleteCard);
+
 cardsRouter.post('/cards', cors(), celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().pattern(urlRegexpPattern),
   }),
 }), createCard);
+
+cardsRouter.options('/cards/:id/likes', cors());
 cardsRouter.put('/cards/:id/likes', cors(), celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().alphanum().length(24),
   }),
 }), likeCard);
+
+cardsRouter.options('/cards/:id/likes', cors());
 cardsRouter.delete('/cards/:id/likes', cors(), celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().alphanum().length(24),
--- routes/users.js
+++ routes/users.js
@@ -10,21 +10,27 @@ const {
 } = require('../controllers/users');
 const urlRegexpPattern = require('../regexp');
 
-usersRouter.options('*', cors());
+// usersRouter.options('*', cors());
 
 usersRouter.get('/users', cors(), getUsers);
+
 usersRouter.get('/users/me', cors(), getUserMe);
+
 usersRouter.get('/users/:id', cors(), celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().alphanum().length(24),
   }),
 }), getUser);
+
+usersRouter.options('/users/me', cors());
 usersRouter.patch('/users/me', cors(), celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
+
+usersRouter.options('/users/me/avatar', cors());
 usersRouter.patch('/users/me/avatar', cors(), celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().pattern(urlRegexpPattern),
